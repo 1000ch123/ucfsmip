2f1ba89226275d1d0ed46dc5463035967490970f
masa <masa@cyber.t.u-tokyo.ac.jp>
2015-05-17 15:14:17 +0900

    while文の実行

--- ch_02/simple.rb
+++ ch_02/simple.rb
@@ -200,6 +200,27 @@ class Sequence < Struct.new(:first, :second)
     end
 end
 
+## while文
+## 再帰的な定義だ
+class While < Struct.new(:condition, :body)
+    def to_s
+        "while (#{condition}) { #{body} }"
+    end
+
+    def inspect
+        "«#{self}»"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce(environment)
+        [If.new(condition, Sequence.new(body, self), DoNothing.new), environment]
+    end
+end
+
+
 # 仮想機械
 class Machine < Struct.new(:expression,:environment)
     def step
