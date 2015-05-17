ab4b76b50386a64a5db2ae746a6339881b402d0f
masa <masa@cyber.t.u-tokyo.ac.jp>
2015-05-17 14:53:27 +0900

    代入文の実行

--- ch_02/simple.rb
+++ ch_02/simple.rb
@@ -1,3 +1,4 @@
+# 対象
 class Number < Struct.new(:value)
     def to_s
         value.to_s
@@ -12,6 +13,46 @@ class Number < Struct.new(:value)
     end
 end
 
+
+class Boolean < Struct.new(:value)
+    def to_s
+        value.to_s
+    end
+
+    def inspect
+        "«#{self}»"
+    end
+
+    def reducible?
+        false
+    end
+end
+
+class LessThan < Struct.new(:left, :right)
+    def to_s
+        "#{left} < #{right}"
+    end
+
+    def inspect
+        "«#{self}»"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce(environment)
+        if left.reducible?
+            LessThan.new(left.reduce(environment), right)
+        elsif right.reducible?
+            LessThan.new(left, right.reduce(environment))
+        else
+            Boolean.new(left.value < right.value)
+        end
+    end
+end
+
+# 演算子
 class Add < Struct.new(:left,:right)
     def to_s
         "#{left} + #{right}"
@@ -60,7 +101,49 @@ class Multiply < Struct.new(:left,:right)
     end
 end
 
+# 文
+class DoNothing
+    def to_s
+        "do-nothing"
+    end
 
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def ==(other_statement)
+        other_statement.instance_of?(DoNothing)
+    end
+
+    def reducible?
+        false
+    end
+end
+
+## 代入文
+class Assign < Struct.new(:name, :expression)
+    def to_s
+        "#{name} = #{expression}"
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce(environment)
+        if expression.reducible?
+            [Assign.new(name, expression.reduce(environment)),environment]
+        else
+            [DoNothing.new, environment.merge({name => expression})]
+        end
+    end
+end
+
+# 仮想機械
 class Machine < Struct.new(:expression,:environment)
     def step
         self.expression = expression.reduce(environment)
@@ -74,6 +157,22 @@ class Machine < Struct.new(:expression,:environment)
         puts expression
     end
 end
+
+class StatementMachine < Struct.new(:statement,:environment)
+    def step
+        self.statement, self.environment = statement.reduce(environment)
+    end
+
+    def run
+        while statement.reducible?
+            puts "#{statement}, #{environment}"
+            step
+        end
+        puts "#{statement}, #{environment}"
+    end
+end
+
+
 
 class Variable < Struct.new(:name)
     def to_s
@@ -95,14 +194,22 @@ end
 
 
 # 簡約計算
+puts "1:加算"
 exp = Add.new(
     Multiply.new(Number.new(1),Number.new(2)),
     Multiply.new(Number.new(3),Number.new(4))
 )
 
 env = {}
-
 Machine.new(exp,env).run
+
+puts "\n比較演算"
+exp = LessThan.new(
+    Number.new(5),
+    Add.new(Number.new(2), Number.new(2))
+)
+Machine.new(exp,env).run
+
 
 # 変数を使う
 exp = Add.new(
@@ -114,4 +221,17 @@ env = {
     y: Number.new(5)
 }
 
+puts "\n変数"
 Machine.new(exp,env).run
+
+# 文を扱う
+state = Assign.new(
+    :x, Add.new(Variable.new(:x), Number.new(1))
+)
+
+env = {
+    x: Number.new(2)
+}
+
+puts "\n代入文"
+StatementMachine.new(state, env).run
